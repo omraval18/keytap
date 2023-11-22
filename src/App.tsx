d6eaee4db75a17aa0ee27fcd9e13185f1cec2a1a
Om Raval <r.auckwell18@gmail.com>
2023-11-22 17:01:52 +0530

    tips added

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,34 @@ const App: FC = () => {
                 style={{
                     position: "absolute",
                     top: "0",
+                    left: "0",
+                    marginLeft: "20px",
+                    marginTop: "20px",
+                    display: "flex",
+                    alignItems: "center",
+                    fontSize: "10px",
+                    color: "GrayText",
+                }}
+            >
+                {" "}
+                Press &nbsp;
+                <p
+                    style={{
+                        padding: "2px",
+                        border: "solid 0.5px grey",
+                        borderRadius: "2px",
+                        background: "grey",
+                        color: "black",
+                    }}
+                >
+                    enter
+                </p>{" "}
+                &nbsp; to start Typing
+            </p>
+            <p
+                style={{
+                    position: "absolute",
+                    top: "0",
                     right: "0",
                     marginRight: "20px",
                     marginTop: "20px",
